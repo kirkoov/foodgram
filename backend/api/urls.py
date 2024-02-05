644abe082a05a81016aefe849eaf6d11ecf348ab
Кирилл Кувшинов <k.koovshinov@gmail.com>
2024-02-05 17:25:55 +0300

    Update the Tag tests & pytest.ini specs.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,13 +5,14 @@ from django.urls import include, path
 from api.views import TagViewSet
 
 
+app_name = "api"
+
+
 router_v1 = routers.DefaultRouter()
-router_v1.register(r"tags", TagViewSet)
+router_v1.register(r"tags", TagViewSet, basename="tags")
 
 urlpatterns = [
     path(r"auth", include("djoser.urls")),
     path(r"auth", include("djoser.urls.authtoken")),
     path(r"", include(router_v1.urls)),
 ]
-
-# path("users/", include("users.urls", namespace="users"))
--- backend/recipes/tests.py
+++ backend/recipes/tests.py
@@ -1,4 +1,6 @@
+from sys import maxsize
 import pytest
+from rest_framework.test import APIRequestFactory
 
 from django.conf import settings
 from django.core.exceptions import ValidationError
@@ -7,15 +9,30 @@ from django.test import TestCase
 
 from .models import Tag
 from .validators import validate_hex_color, validate_slug_field
+from api.views import TagViewSet
 
 
-class RecipeTests(TestCase):
+class TagTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.factory = APIRequestFactory()
+        cls.test_tags = []
+        cls.request_detail = cls.factory.get("/api/tags/0/")
+        for index in range(3):
+            tag = Tag(
+                name=f"Tag {index}",
+                color=f"#E26C{index}D",
+                slug=f"-{index}_",
+            )
+            cls.test_tags.append(tag)
+        Tag.objects.bulk_create(cls.test_tags)
+
     def test_create_tag_name(self):
         with pytest.raises(DataError):
             Tag.objects.create(
                 name=settings.NUM_CHARS_MEALTIME_NAME * "s" + "more",
                 color=None,
-                slug="",
+                slug=None,
             )
 
     def test_create_tag_color(self):
@@ -29,6 +46,44 @@ class RecipeTests(TestCase):
     def test_create_tag_slug(self):
         Tag.objects.create(
             name=settings.NUM_CHARS_MEALTIME_NAME * "s",
-            color="",
+            color=None,
             slug=validate_slug_field("a-proper-slug"),
         )
+
+    def test_get_taglist(self):
+        request = self.factory.get("/api/tags/")
+        view = TagViewSet.as_view({"get": "list"})
+        response = view(request)
+        data = response.__dict__.get("data")
+        if data is not None:
+            for new, default in zip(data, self.test_tags):
+                assert new["name"] == default.name
+                assert new["color"] == default.color
+                assert new["slug"] == default.slug
+        else:
+            raise DataError(
+                "Empty data in the test_get_taglist() ordered dicts."
+            )
+
+    def test_get_tagdetail(self):
+        view = TagViewSet.as_view({"get": "retrieve"})
+        response = view(self.request_detail, pk=1)
+        data = response.__dict__.get("data")
+        if data is not None:
+            assert data["name"] == self.test_tags[0].name
+            assert data["color"] == self.test_tags[0].color
+            assert data["slug"] == self.test_tags[0].slug
+        else:
+            raise DataError(
+                "Empty data in the test_get_tagdetail() ordered dicts."
+            )
+
+    def test_get_tagdetail_status200(self):
+        view = TagViewSet.as_view({"get": "retrieve"})
+        response = view(self.request_detail, pk=1)
+        assert response.status_code == 200
+
+    def test_get_tagdetail_status404(self):
+        view = TagViewSet.as_view({"get": "retrieve"})
+        response = view(self.request_detail, pk=maxsize)
+        assert response.status_code == 404
