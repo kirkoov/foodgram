f6485dfabae08cec53a6dd15ded8a832aff4c0f9
Кирилл Кувшинов <k.koovshinov@gmail.com>
2024-02-26 13:44:05 +0300

    Tidy up the users' admin zone by unregistering tokens & groups. Unclear about the subscriptions.

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,22 +1,37 @@
+from rest_framework.authtoken.models import TokenProxy
+
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 from django.utils.translation import gettext_lazy as _
 
 from .models import User, Subscription
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UsersAdmin(UserAdmin):
     list_display = (
         "pk",
         "email",
         "username",
         "first_name",
-        "last_name",
-        "is_staff",
-        "is_superuser",
+        "count_recipes",
+        "count_subscriptions",
     )
     list_filter = ("email", "username")
     empty_value_display = _("empty")
+
+    @admin.display(
+        description=_("recipes"),
+    )
+    def count_recipes(self, obj):
+        return obj.recipes.count()
+
+    @admin.display(
+        description=_("subscriptions"),
+    )
+    def count_subscriptions(self, obj):
+        return obj.is_subscriber.count()
 
 
 @admin.register(Subscription)
@@ -26,3 +41,7 @@ class SubscriptionAdmin(admin.ModelAdmin):
         "user",
         "author",
     )
+
+
+admin.site.unregister(Group)
+admin.site.unregister(TokenProxy)
