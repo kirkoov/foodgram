41bc65e2e709f834a060777d7b8dfeb70213f9c9
Кирилл Кувшинов <k.koovshinov@gmail.com>
2024-02-26 07:31:50 +0300

    Get the linters do their checks.

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.db.models import Count
+
+# from django.db.models import Count
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
