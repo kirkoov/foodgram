4e867a78fcf87c71f9207f1ff89ad200ee693363
Кирилл Кувшинов <k.koovshinov@gmail.com>
2024-03-14 12:55:00 +0300

    Ditto.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -69,7 +69,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8090",
 ]
-CORS_URLS_REGEX = r"^/api/.*$", r"^/admin/.*$"
+CORS_URLS_REGEX = r"^/api/.*$"
 
 ROOT_URLCONF = "backend.urls"
 
