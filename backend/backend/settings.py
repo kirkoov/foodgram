25f2845fd14e1e9ee57132247b859a6e34e37f4b
Кирилл Кувшинов <k.koovshinov@gmail.com>
2024-04-01 14:14:55 +0300

    Comment the DjDT for the prod case.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
