2a653284101ce60270e67a2ec2b9327f52b34f41
Кирилл Кувшинов <k.koovshinov@gmail.com>
2024-09-23 16:37:18 +0300

    Finishing with the users unittets for now.

--- backend/users/tests/test_logic.py
+++ backend/users/tests/test_logic.py
@@ -1,4 +1,5 @@
 import json
+from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
@@ -9,7 +10,6 @@ User = get_user_model()
 
 
 class TestLogic(TestCase):
-    TOTAL = constants.TEST_NUM_USERS
     DATA = constants.TEST_USER_DATA
     client = Client()
 
@@ -29,7 +29,7 @@ class TestLogic(TestCase):
             constants.TEST_USER_PWD_CHANGE,
             data=data,
         )
-        self.assertIn("detail", json.loads(response.content))
+        self.assertEqual(response.status_code, HTTPStatus.UNAUTHORIZED)
 
     def test_anonymous_user_cant_get_delete_token(self):
         fake_data = dict(TestLogic.DATA)
@@ -38,12 +38,41 @@ class TestLogic(TestCase):
             constants.TEST_USER_TOKEN_ON_URL,
             data=fake_data,
         )
-        # fmt: off
-        self.assertIn(
-            "non_field_errors",
-            json.loads(response.content)
-        )
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
         response = self.client.post(
             constants.TEST_USER_TOKEN_OFF_URL,
         )
-        self.assertIn("detail", json.loads(response.content))
+        self.assertEqual(response.status_code, HTTPStatus.UNAUTHORIZED)
+
+    def test_user_cant_use_anothers_data(self):
+        token_d = self.get_token(TestLogic.DATA)
+        user_count_ini = User.objects.count()
+        anothers_data = dict(TestLogic.DATA)
+        anothers_data["username"] = "trangerCoco23"
+        anothers_data["password"] = "VeRY02strangelYd_ifFernt~"
+        anothers_data["email"] = "elYd@ya.org"
+        self.client.post(
+            constants.TEST_USERS_PAGE_URL,
+            data=anothers_data,
+        )
+        self.assertEqual(User.objects.count(), user_count_ini + 1)
+
+        pwd_change_data = {
+            "new_password": anothers_data["password"][1:],
+            "current_password": anothers_data["password"],
+        }
+        response = self.client.post(
+            constants.TEST_USER_PWD_CHANGE,
+            data=pwd_change_data,
+            headers={
+                "Authorization": f"Token {token_d[constants.AUTH_TOKEN_FIELD]}",
+            },
+        )
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+
+    def get_token(self, user_data) -> str:
+        response = self.client.post(
+            constants.TEST_USER_TOKEN_ON_URL,
+            data=user_data,
+        )
+        return json.loads(response.content)
