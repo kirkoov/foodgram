d2a2172282da21b5115ff5b3da648a3148aebf66
Кирилл Кувшинов <k.koovshinov@gmail.com>
2025-12-16 10:24:43 +0200

    Added explanations to the fron's components/footer/footer.js which seems to poorly sync with the Docker img. Forcing now.

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,6 +1,11 @@
 import styles from './style.module.css'
 import { Container, LinkComponent } from '../index'
 
+// Add a comment here to force a commit and push:
+// This line forces Git to see a change.
+// This line likely existed but was never committed or so
+//const { isFetching, isError, error, isSuccess, isIdle, data, isPaused } = useQuery('...');
+
 const Footer = () => {
   return <footer className={styles.footer}>
       <Container className={styles.footer__container}>
